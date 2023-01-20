e7da094dfc84676d007388be5918104e63890043
adou <julandry1@gmail.com>
2023-01-20 01:17:41 +0000

    02 modif

--- commande/urls.py
+++ commande/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('',views.list_commande, name='listcommande'),
     path('ajout_commande',views.ajouter_commande, name='ajout_commande'),
     path('modifier_commande/<str:pk>',views.modifier_command, name='modifier_commande'),
+    path('supprimer_commande/<str:pk>',views.supprimer_command, name='supprimer_commande'),
     path('supprimer_commande/<str:pk>',views.supprimer_command, name='supprimer_commande')
 ]
